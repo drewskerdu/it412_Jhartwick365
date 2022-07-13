07f005ec149c1ef94604aae7a992a894fb17f08e
Joseph Hartwick <Jhartwic@mail.walshcollege.edu>
2022-07-13 12:36:23 -0400

    completed lecture code

---
+++ Week1ClassesAssignment/Classes/cat.py
@@ -0,0 +1,7 @@
+from Classes.pet import Pet
+
+class Cat(Pet):
+    """A simple class for representing a cat"""
+    def __init__(self, name, age):
+        """Initialize name and age variables/attributes for the cat"""
+        super().__init__(name, age)
---
+++ Week1ClassesAssignment/classes_test2.py
@@ -0,0 +1,16 @@
+from Classes.pet import Pet
+from Classes.dog import Dog
+from Classes.cat import Cat
+    
+
+
+
+
+
+my_dog = Dog("Scout", 3, "German Shepherd")
+print("My dogs name is: " + my_dog.name)
+my_dog.clean()
+print("My dogs breed is a " + my_dog.breed)
+
+my_cat = Cat("Fluffy", 3)
+print("My dogs name is: " + my_cat.name)
